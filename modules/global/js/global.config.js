39a591319a95cb3fd862595795088264666cf553
Legkodymov Lev <legkodymov.lev@gmail.com>
2015-07-11 08:41:47 +0300

    Отверстал шапку и сетку для калькулятора

--- modules/global/js/global.config.js
+++ modules/global/js/global.config.js
@@ -1,2 +1,17 @@
-angular.module('global', ['ngMaterial']);
+(function(){
+	'use strict';
+
+	angular.module('global', ['ngMaterial'])
+		.config(config);
+
+	config.$inject = ['$mdThemingProvider'];
+
+	function config($mdThemingProvider){
+		$mdThemingProvider.theme('default')
+			.primaryPalette('indigo')
+			.accentPalette('blue');
+	}
+})();
+
+
     
