215ed863099a4bd98560f2a0d5b4acf1e58c1fab
Legkodymov Lev <legkodymov.lev@gmail.com>
2015-08-26 06:47:17 +0300

    Добавлена шапка странички услуг, добавлены скидки

--- modules/global/js/global.config.js
+++ modules/global/js/global.config.js
@@ -1,37 +1,44 @@
 (function(){
 	'use strict';
 
-	angular.module('global', ['ngMaterial'])
+	angular.module('global', ['ngMaterial', 'ui.router'])
 		.config(config);
 
-	config.$inject = ['$mdThemingProvider'];
+	config.$inject = ['$mdThemingProvider', '$urlRouterProvider', '$stateProvider'];
 
-	function config($mdThemingProvider){
-		$mdThemingProvider.definePalette('dark-green', {
-		    '50': 'rgba(48, 178, 55, 1)',
-		    '100': 'rgba(48, 178, 55, 1)',
-		    '200': 'rgba(48, 178, 55, 1)',
-		    '300': 'rgba(48, 178, 55, 1)',
-		    '400': 'rgba(48, 178, 55, 1)',
-		    '500': 'rgba(48, 178, 55, 1)',
-		    '600': 'rgba(48, 178, 55, 1)',
-		    '700': 'rgba(48, 178, 55, 1)',
-		    '800': 'rgba(48, 178, 55, 1)',
-		    '900': 'rgba(48, 178, 55, 1)',
-		    'A100': 'rgba(48, 178, 55, 1)',
-		    'A200': 'rgba(48, 178, 55, 1)',
-		    'A400': 'rgba(48, 178, 55, 1)',
-		    'A700': 'rgba(48, 178, 55, 1)',
-		    'contrastDefaultColor': 'light',    // whether, by default, text (contrast)
-		                                        // on this palette should be dark or light
-		    'contrastDarkColors': ['50', '100', //hues which contrast should be 'dark' by default
-		     '200', '300', '400', 'A100'],
-		    'contrastLightColors': undefined    // could also specify this if default was 'dark'
-		  });
+	function config($mdThemingProvider, $urlRouterProvider, $stateProvider){
 
 		$mdThemingProvider.theme('default')
-			.primaryPalette('blue')
-			.accentPalette('dark-green');
+			.primaryPalette('blue-grey');
+
+
+		$stateProvider
+			.state('contacts', {
+				"url": "/contacts",
+				"views": {
+					"content": {
+						"templateUrl": "views/contacts/contacts.html"
+					}
+				}
+			})
+
+			.state('company', {
+				"url": "/company",
+				"views": {
+					"content": {
+						"templateUrl": "views/company/company.html"
+					}
+				}
+			})
+
+			.state('services', {
+				"url": "/services",
+				"views": {
+					"content": {
+						"templateUrl": "views/services/services.html"
+					}
+				}
+			})
 	}
 })();
 
